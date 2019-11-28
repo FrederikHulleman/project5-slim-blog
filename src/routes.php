afbcf1976ab83fd563db821723c1bb0a08908050
Frederik Hulleman <fhulleman@gmail.com>
2019-11-28 10:18:12 +0100

    Created base.twig and cleaned up not required files

--- src/routes.php
+++ src/routes.php
@@ -6,30 +6,21 @@ use Project5SlimBlog\CommentMapper;
 $app->get('/post/{id}', function ($request, $response, $args) {
 
     $post_id = (int)$args['id'];
-    $this->logger->addInfo("Post: $post_id");
+    $this->logger->info("Post: $post_id");
     $mapper = new PostMapper($this->db);
     $mapper->selectPosts($post_id);
     return $this->view->render($response, 'detail.twig', [
      'post' => $mapper->posts[0]
     ]);
-    // foreach ($mapper->posts as $post) {
-    //   $response->getBody()->write(var_export($mapper->posts[0], true));
-    //
-    // }
-    // return $response;
 })->setName('post-detail');
 
 $app->get('/[{posts}]', function ($request, $response, $args) {
-    $this->logger->addInfo("Posts list");
+    $this->logger->info("Posts list");
     $mapper = new PostMapper($this->db);
     $mapper->selectPosts();
-    return $this->view->render($response, 'index.twig', [
+    return $this->view->render($response, 'blog.twig', [
       'posts' => $mapper->posts
     ]);
-    // foreach ($mapper->posts as $post) {
-    //   $response->getBody()->write(var_export($post, true));
-    // }
-    // return $response;
 })->setName('posts-list');
 
 // $app->get('/[{name}]', function ($request, $response, $args) {
