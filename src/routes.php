044a8ac3a8a8e4c4d94e0dd53f3bd46e587c7e81
Frederik Hulleman <fhulleman@gmail.com>
2019-12-02 12:18:31 +0100

    1st setup for new posts: routing & templates

--- src/routes.php
+++ src/routes.php
@@ -3,6 +3,46 @@
 use Project5SlimBlog\PostMapper;
 use Project5SlimBlog\CommentMapper;
 
+$app->map(['GET','POST'],'/post/new', function ($request, $response, $args) {
+
+  if($request->getMethod() == "POST") {
+
+    $args = array_merge($args, $request->getParsedBody());
+    $args = filter_var_array($args,FILTER_SANITIZE_STRING);
+
+    $log = json_encode(["title: ".$args['title'],"body: ".$args['body']]);
+    if(!empty($args['title']) && !empty($args['body'])) {
+      $post_mapper = new PostMapper($this->db);
+
+      if($post_mapper->insert($args)) {
+          $this->logger->notice("New post: SUCCESFUL | $log");
+          //to avoid resubmitting values:
+          $url = $this->router->pathFor('posts-list');
+          return $response->withStatus(302)->withHeader('Location',$url);
+        } else {
+          $args['error'] = $post_mapper->getAlert()[0]['message'];
+          $this->logger->notice("New post: UNSUCCESFUL | $log");
+        }
+      }
+      else {
+        $args['error'] = "all fields required";
+        $this->logger->notice("New post: UNSUCCESFUL | $log");
+      }
+  }
+
+  $nameKey = $this->csrf->getTokenNameKey();
+  $valueKey = $this->csrf->getTokenValueKey();
+  $csrf = [
+    $nameKey => $request->getAttribute($nameKey),
+    $valueKey => $request->getAttribute($valueKey)
+  ];
+
+  return $this->view->render($response, 'new.twig', [
+   'csrf' => $csrf,
+   'args' => $args
+  ]);
+})->setName('new-post');
+
 $app->map(['GET','POST'],'/post/{post_id}', function ($request, $response, $args) {
   $post_id = (int)$args['post_id'];
 
